c10d0086efcf25d63d5ff31cf64a10ffeef0b33b
dcamo0217 <dcavadia@uninorte.edu.co>
2022-11-20 17:30:26 -0500

    add 03-detalles

---
+++ 03-detalles/src/components/Calculadora.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NumberInput from './NumberInput';
+
+
+const Calculadora = () => {
+    
+  return (
+    <div>
+      <NumberInput />
+    </div>
+  )
+}
+
+export default Calculadora
