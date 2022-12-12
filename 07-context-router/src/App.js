c4178b053d831ec5ca0c185bf21a6d3a06c440f7
dcamo0217 <dcavadia@uninorte.edu.co>
2022-12-12 13:40:23 -0500

    load: add 07-context-router

---
+++ 07-context-router/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { UserContext } from "./contexts/UserContext";
+import AppRouter from "./routes/AppRouter";
+
+const App = () => {
+    const [user, setUser] = useState(null);
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            <AppRouter />
+        </UserContext.Provider>
+    );
+};
+
+export default App;
---
+++ 07-context-router/src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <h1 className="text-center mt-3 text-danger">
+            Error 404: Page not found
+        </h1>
+    );
+};
+
+export default NotFound;
